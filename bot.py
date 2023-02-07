4ba3b34e196c4264fa4a340d3dfa4eabafe04f14
hipsterwaldo <maxiwaldner@googlemail.com>
2023-02-07 15:32:05 +0100

    first commit

---
+++ bot.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+import decksearch
+from lor_deckcodes import LoRDeck
+
+def run_discord_bot():
+    TOKEN = 'OTAyNjU1MDA0MTQwMjQwOTE2.Gq8FLG.lImLswI0TkUIFNt1ysbhe0l_dvLnaHNX8yzehk'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = commands.Bot(command_prefix="!",  intents=intents)
+
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    
+    @client.event
+    async def on_message(message):
+
+        if message.content.startswith("!deck"):
+            
+            split_message = message.content.split()
+            deck = LoRDeck.from_deckcode(split_message[1])
+            sorted_deck = decksearch.sort_for_cost(deck)
+            url="https://runeterra.ar/decks/code/" + split_message[1]
+            champions = decksearch.card_list_to_string(decksearch.get_champions(sorted_deck))
+            units = decksearch.card_list_to_string(decksearch.get_units(sorted_deck))
+            spells = decksearch.card_list_to_string(decksearch.get_spells(sorted_deck))
+            other = decksearch.card_list_to_string(decksearch.get_other(sorted_deck))
+
+            embed=discord.Embed(title="Decklink runeterra.ar", url=url)
+            embed.set_thumbnail(url="https://static.wikia.nocookie.net/leagueoflegends/images/2/2c/Legends_of_Runeterra_icon.png/revision/latest?cb=20191020214918")
+            embed.add_field(name="Champions", value=champions, inline=True)
+            embed.add_field(name="Units", value=units, inline=True)
+            embed.add_field(name="Spells", value=spells, inline=True)
+            embed.add_field(name="Equipments and Landmarks", value=other, inline=True)
+
+            await message.channel.send(embed=embed)
+
+        if message.content.startswith("!card"):
+            split_message = message.content.split(" ", 2)
+            print(split_message)
+            url = decksearch.get_url_path(split_message[1])
+
+            embed=discord.Embed(title=split_message[1].capitalize())
+            embed.set_image(url=url)
+            await message.channel.send(embed=embed)
+
+    
+    client.run(TOKEN)
---
+++ decksearch.py
@@ -0,0 +1,121 @@
+from lor_deckcodes import LoRDeck, CardCodeAndCount
+import json
+from typing import List
+import pathlib
+from dataclasses import dataclass
+
+# loading the sets
+MAIN_DATA_PATH = pathlib.Path(r"B:\Eigene Dateien\desktop\bard bot\lor_decks")
+
+
+deck = LoRDeck.from_deckcode('CUDQCAQAAIAQKAAMAEDACEQBAYCAQAIGA4CAEAIADUWQIBQACEMBWHIDAEBAAAIBAYAAWAIGBQNACAIBAASQ')
+card = deck.cards[1]
+
+def load_all_sets(MAIN_DATA_PATH):
+    set1_path = MAIN_DATA_PATH / "set1-lite-en_us\en_us\data\set1-en_us.json"
+    set2_path = r"B:\Eigene Dateien\desktop\bard bot\lor_decks\set2-lite-en_us\en_us\data\set2-en_us.json"
+    set3_path = r"B:\Eigene Dateien\desktop\bard bot\lor_decks\set3-lite-en_us\en_us\data\set3-en_us.json"
+    set4_path = r"B:\Eigene Dateien\desktop\bard bot\lor_decks\set4-lite-en_us\en_us\data\set4-en_us.json"
+    set5_path = r"B:\Eigene Dateien\desktop\bard bot\lor_decks\set5-lite-en_us\en_us\data\set5-en_us.json"
+    set6_path = r"B:\Eigene Dateien\desktop\bard bot\lor_decks\set6-lite-en_us\en_us\data\set6-en_us.json"
+    set7_path = r"B:\Eigene Dateien\desktop\bard bot\lor_decks\set6cde-lite-en_us\en_us\data\set6cde-en_us.json"
+    all_sets = []
+# storing sets from the json
+    for i in range(0, 7):
+        all_sets_path = [set1_path, set2_path, set3_path, set4_path, set5_path,
+        set6_path, set7_path]
+        with open(all_sets_path[i], "r", encoding='utf8') as f:
+            all_sets.append(json.load(f))
+
+    return all_sets
+
+#
+# search function that searches for the dictionary of a single inserted card code(01FR036)
+# 
+# @returns: dictionary of the given card code
+#
+def get_card_dictionary(card, all_sets):
+    if card.set == 6:
+        for card_dictionary in all_sets[5]:
+            if card_dictionary.get("cardCode") == card.card_code:               
+                return card_dictionary
+        for card_dictionary in all_sets[6]:
+            if card_dictionary.get("cardCode") == card.card_code:               
+                return card_dictionary
+
+    for card_dictionary in all_sets[card.set-1]:
+        if card_dictionary.get("cardCode") == card.card_code:   
+            return card_dictionary
+
+    raise RuntimeError(f"Cannot find {card} in the given sets.")
+
+#
+# sorts a decklist for its card costs 
+#
+# @returns: a list of tuples with the sorted cards dictionarys and their respective number of copies
+#
+def sort_for_cost(deck, all_sets):    
+    card_list = [(get_card_dictionary(card, all_sets), card.count) for card in deck.cards]
+    sorted_card_list = sorted(card_list, key=lambda x:x[0]["cost"])
+    return sorted_card_list
+
+#
+# get functions that search for the respective card type and puts them into a
+# list [number of copies + x, card name]
+#
+# @returns: list with type of cards
+#
+def get_champions(deck):
+    champion_list = []
+    for card in deck:
+        if card[0]["supertype"] == "Champion":
+            champion_list.append(str(card[1]) + "x " + card[0]["name"])
+    return champion_list
+
+def get_units(deck):
+    list = []
+    for card in deck:
+        if card[0]["supertype"] != "Champion" and card[0]["type"] == "Unit":
+            list.append(str(card[1]) + "x " + card[0]["name"])
+    return list
+
+def get_other(deck):
+    other_list = []
+    for card in deck:
+        if card[0]["type"] == "Equipment" or  card[0]["type"] == "Landmark":
+            other_list.append(str(card[1]) + "x " + card[0]["name"])
+    return other_list
+
+def get_spells(deck):
+    spells_list = []
+    for card in deck:
+        if card[0]["type"] == "Spell":
+            spells_list.append(str(card[1]) + "x " + card[0]["name"])
+    return spells_list
+
+#
+# read in list of cards
+#
+# @returns: string of inserted list
+#
+def card_list_to_string(card_list):
+    word = ""
+    for card in card_list:
+        word += card + "\n"
+    return word
+
+#
+# search function that inserts a cards name and
+# searches for the card image in it's respective dictionary
+#
+# @returns: the url of a cards respective card Image
+#
+def get_url_path(card_name, all_sets):
+    card_name = card_name.lower()  
+    for card_set in all_sets:
+        for card_dictionary in card_set:
+            if card_dictionary["name"].lower() == card_name:
+                card_path = card_dictionary["assets"][0]["gameAbsolutePath"]
+                return card_path
+    raise RuntimeError(f"Cannot find {card_name} in all_sets")
+
